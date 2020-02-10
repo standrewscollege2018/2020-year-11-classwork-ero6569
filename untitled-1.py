1ea1e703d921efadeb9655c08692416cc2326d93
ero6569 <60585428+ero6569@users.noreply.github.com>
2020-02-10 14:14:20 +1300

    Create untitled-1.py

---
+++ untitled-1.py
@@ -0,0 +1,21 @@
+""" My Madlib """
+
+print=input("Enter name")
+
+body_parrt = input ("Enter boy part")
+
+illness = input("Enter illness")
+
+school_subject = input("Enter subject")
+
+number  = float(input("Enter a number greater than 5"))
+
+Print(" Thinking...here is my madlib")
+
+Print("To whom it may concern")
+
+Print("Please excuse", name, "from", subject, "today. He is suffering from a bad case of", illness, "which as caused a lot of", body_part, "pain.", name, "should recover soon he will be back at school in 5 days")
+      
+ Print("kind regards,")     
+ 
+ Print(name "/'s dad") 
