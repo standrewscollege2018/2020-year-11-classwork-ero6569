8aaae925d0f40c81a2d0c54226254a8b61639c38
ero6569 <60585428+ero6569@users.noreply.github.com>
2020-02-13 11:11:10 +1300

    Create untitled-2.py

---
+++ untitled-2.py
@@ -0,0 +1,21 @@
+""" This program shows what mark you have recived in a test """
+
+if grade greater than 100 or  grade less then 0 
+print("That is not a valid score")
+
+Grade>= 90:
+
+input("what is your grade?")
+
+=if user =>90< :
+(print("You have recieved an A grade"))
+
+=elif user =<=70:
+    print("You have recived a B")
+    
+=elif user =>50:
+    print("You have recived a C")
+        
+=elif user =<50:
+    print("You have failed")
+            
